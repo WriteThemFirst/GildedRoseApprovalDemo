29536f6a9b8f8617b620c6d83fee527aecb1f9cd
Michel Daviot <michel@daviot.info>
2018-02-27 23:59:45 +0100

    :tada: first project using approvals-java

---
+++ README.md
@@ -0,0 +1,4 @@
+# Sample project using approval testing
+
+Uses the library [approvals-java](https://github.com/WriteThemFirst/approvals-java)
+ to test the Gilded Rose legacy code project.
---
+++ src/test/java/com/gildedrose/GildedRoseApprovalTest.java
@@ -0,0 +1,29 @@
+package com.gildedrose;
+
+
+import com.github.writethemfirst.approvals.Approvals;
+import org.junit.jupiter.api.Test;
+
+import static com.gildedrose.ItemNames.BRIE;
+
+
+class GildedRoseApprovalTest {
+
+    private Approvals approvals = new Approvals();
+
+    @Test
+    void updateQuality_normal_shouldDecrease() throws Exception {
+        Item item = doTest(BRIE, -2, 10);
+        approvals.verify(item);
+    }
+
+    private Item doTest(String name, int sellIn, int quality) {
+        //GIVEN
+        Item[] items = new Item[]{new Item(name, sellIn, quality)};
+        GildedRose app = new GildedRose(items);
+        //WHEN
+        app.updateQuality();
+        return app.items[0];
+    }
+
+}
