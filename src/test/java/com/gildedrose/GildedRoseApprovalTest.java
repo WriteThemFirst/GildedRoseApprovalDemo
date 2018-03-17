d8d06c2c8f79405186248c6ad9ca9ff1641bb884
Michel Daviot <michel@daviot.info>
2018-03-17 15:41:36 +0100

    :sparkles: Demonstrate the usage of verifyAll to completely test the Gilded Rose

--- src/test/java/com/gildedrose/GildedRoseApprovalTest.java
+++ src/test/java/com/gildedrose/GildedRoseApprovalTest.java
@@ -4,7 +4,8 @@ package com.gildedrose;
 import com.github.writethemfirst.approvals.Approvals;
 import org.junit.jupiter.api.Test;
 
-import static com.gildedrose.ItemNames.BRIE;
+import static com.gildedrose.ItemNames.*;
+import static java.util.Arrays.asList;
 
 
 class GildedRoseApprovalTest {
@@ -15,6 +16,42 @@ class GildedRoseApprovalTest {
     void updateQuality_normal_shouldDecrease() {
         Item item = doTest(BRIE, -2, 10);
         approvals.verify(item);
+    }
+
+    @Test
+    void updateQuality_normal_shouldDecrease_all() {
+        approvals.verifyAll(
+            asList("normal item"),
+            asList(-1, 0, 1, 10),
+            asList(-1, 0, 1, 10),
+            this::doTest);
+    }
+
+    @Test
+    void updateQuality_brie_shouldDecrease_all() {
+        approvals.verifyAll(
+            asList(BRIE),
+            asList(-1, 0, 1, 10),
+            asList(-1, 0, 1, 10),
+            this::doTest);
+    }
+
+    @Test
+    void updateQuality_sulfuras_shouldStay() {
+        approvals.verifyAll(
+            asList(SULFURAS),
+            asList(-1, 0, 1, 10),
+            asList(-1, 0, 1, 50, 51),
+            this::doTest);
+    }
+
+    @Test
+    void updateQuality_pass_shouldEvolve() {
+        approvals.verifyAll(
+            asList(PASS),
+            asList(-1, 0, 1, 5, 6, 10, 11),
+            asList(-1, 0, 1, 10),
+            this::doTest);
     }
 
     private Item doTest(String name, int sellIn, int quality) {
