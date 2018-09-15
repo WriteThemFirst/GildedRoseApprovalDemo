f9a813d9db4dec6cf33f84645fe4c25f4a6791ce
Michel Daviot <michel@daviot.info>
2018-09-15 15:38:30 +0200

    :recycle: fix IDEA warnings

--- src/test/java/com/gildedrose/GildedRoseApprovalTest.java
+++ src/test/java/com/gildedrose/GildedRoseApprovalTest.java
@@ -1,15 +1,14 @@
 package com.gildedrose;
 
-
 import org.junit.jupiter.api.Test;
 
 import static com.gildedrose.ItemNames.*;
-import static com.github.writethemfirst.approvals.Approvals.*;
+import static com.github.writethemfirst.approvals.Approvals.verify;
+import static com.github.writethemfirst.approvals.Approvals.verifyAllCombinations;
 import static java.util.Arrays.asList;
-
+import static java.util.Collections.singletonList;
 
 class GildedRoseApprovalTest {
-
 
     @Test
     void updateQuality_normal_shouldDecrease() {
@@ -20,7 +19,7 @@ class GildedRoseApprovalTest {
     @Test
     void updateQuality_normal_shouldDecrease_all() {
         verifyAllCombinations(
-            asList("normal item"),
+            singletonList("normal item"),
             asList(-1, 0, 1, 10),
             asList(-1, 0, 1, 10),
             this::doTest);
@@ -29,7 +28,7 @@ class GildedRoseApprovalTest {
     @Test
     void updateQuality_brie_shouldDecrease_all() {
         verifyAllCombinations(
-            asList(BRIE),
+            singletonList(BRIE),
             asList(-1, 0, 1, 10),
             asList(-1, 0, 1, 10),
             this::doTest);
@@ -38,7 +37,7 @@ class GildedRoseApprovalTest {
     @Test
     void updateQuality_sulfuras_shouldStay() {
         verifyAllCombinations(
-            asList(SULFURAS),
+            singletonList(SULFURAS),
             asList(-1, 0, 1, 10),
             asList(-1, 0, 1, 50, 51),
             this::doTest);
@@ -47,7 +46,7 @@ class GildedRoseApprovalTest {
     @Test
     void updateQuality_pass_shouldEvolve() {
         verifyAllCombinations(
-            asList(PASS),
+            singletonList(PASS),
             asList(-1, 0, 1, 5, 6, 10, 11),
             asList(-1, 0, 1, 10),
             this::doTest);
